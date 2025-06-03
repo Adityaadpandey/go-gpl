00102dce9482b17b3f3d3100e3657d2be6d701e7
Aditya <adityapandeyadp@gmail.com>
2025-06-03 19:23:29 +0530

    initial commit

---
+++ database/database.go
@@ -0,0 +1,79 @@
+package database
+
+import (
+	"context"
+	"log"
+	"time"
+
+	"github.com/adityaadpandey/go-gpl/graph/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+var connectionString = "mongodb://admin:admin123@localhost:27018"
+
+type DB struct {
+	client *mongo.Client
+}
+
+func Connect() *DB {
+	client, err := mongo.NewClient(options.Client().ApplyURI(connectionString))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &DB{
+		client: client,
+	}
+}
+
+func (db *DB) GetJob(id string) *model.JobListing {
+
+	jobCollec := db.client.Database("gpl").Collection("jobs")
+	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	_id, _ := primitive.ObjectIDFromHex(id)
+	filter := bson.M{"_id": _id}
+
+	var jobListing model.JobListing
+	err := jobCollec.FindOne(ctx, filter).Decode(&jobListing)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return &jobListing
+}
+
+func (db *DB) GetJobs() []*model.JobListing {
+
+	var jobListings []*model.JobListing
+	return jobListings
+}
+
+func (db *DB) CreateJobListing(jobinfo model.CreateJobListingInput) *model.JobListing {
+	var returnJobListing model.JobListing
+	return &returnJobListing
+}
+
+func (db *DB) UpdateJobListing(jobID string, jobInfo model.UpdateJobListingInput) *model.JobListing {
+	var jobListing model.JobListing
+
+	return &jobListing
+}
+
+func (db *DB) DeleteJobListing(jobID string) *model.DeleteJobResponse {
+	return &model.DeleteJobResponse{DeleteJobID: jobID}
+}
